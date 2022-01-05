8a4d88809574b7d877c6e636d43d5062467e91db
Daniel Wagner <dtw@dropbox.com>
2022-01-05 18:57:12 +0000

    fast drawing with LUT. Now 7.84 fps - 41ms reading, 50ms processing, 35ms drawing.

--- src/MLX90640-Thermocam.ino
+++ src/MLX90640-Thermocam.ino
@@ -131,10 +131,8 @@ void setup() {
   // Prepare imageData array
   imageData = (uint16_t *)malloc(O_WIDTH * O_HEIGHT * sizeof(uint16_t));
 
-  // get the cutoff points for the color interpolation routines
-  // note this function called when the temp scale is changed
-  setAbcd();
-  drawLegend();
+  // draw the legend rainbow once
+  drawRainbow();
 }
 
 int frames = 0;
@@ -161,19 +159,21 @@ void loop() {
   readFrame();
   tempTimeRead += millis() - tempTimeNow;
   tempTimeNow = millis();
+  
   computeTemps();
   tempTimeProcess += millis() - tempTimeNow;
   tempTimeNow = millis();
+  
+  setTempScale();
   drawPicture();
   if(frames%2 == 1) {
-    setTempScale();
     drawMeasurement();
+    drawLegend();
   }
   tempTimeDraw += millis() - tempTimeNow;
-  frames++;
-  
-}
 
+  frames++; 
+}
 
 // Read pixel data from MLX90640.
 void readFrame() {
@@ -186,133 +186,70 @@ void readFrame() {
 }
 
 void computeTemps() {
-  float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
   float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
-
   float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
 
   MLX90640_CalculateTo(mlx90640Frame, &mlx90640, EMMISIVITY, tr, tempValues);
 }
 
-int row;
-float temp, temp2;
-
-void interpolate() {
-  for (row=0; row<24; row++) {
-    for (x=0; x<O_WIDTH; x++) {
-      temp  = tempValues[(31 - (x/7)) + (row*32) + 1];
-      temp2 = tempValues[(31 - (x/7)) + (row*32)];
-      interpolated[row*7][x] = lerp(temp, temp2, x%7/7.0);
-    }
-  }
-  for (x=0; x<O_WIDTH; x++) {
-    for (y=0; y<O_HEIGHT; y++) {
-      temp  = interpolated[y-y%7][x];
-      temp2 = interpolated[min((y-y%7)+7, O_HEIGHT-7)][x];
-      interpolated[y][x] = lerp(temp, temp2, 1);//y%7/7.0);
-    }
-  }
-}
 
 
-// Linear interpolation
-float lerp(float v0, float v1, float t) {
-  return v0 + t * (v1 - v0);
-}
-
+const uint16_t camColors[] = {
+  0x001F,0x003F,0x005F,0x009F,0x00BF,0x00DF,0x011F,0x013F,0x015F,0x019F,
+  0x01BF,0x01DF,0x021F,0x023F,0x025F,0x029F,0x02BF,0x02DF,0x031F,0x033F,
+  0x035F,0x039F,0x03BF,0x03DF,0x041F,0x043F,0x045F,0x049F,0x04BF,0x04DF,
+  0x051F,0x053F,0x055F,0x059F,0x05BF,0x05DF,0x061F,0x063F,0x065F,0x069F,
+  0x06BF,0x06FF,0x071F,0x073F,0x077F,0x079F,0x07BF,0x07FF,0x07FF,0x07FD,
+  0x07FC,0x07FB,0x07F9,0x07F8,0x07F7,0x07F5,0x07F4,0x07F3,0x07F1,0x07F0,
+  0x07EF,0x07ED,0x07EC,0x07EB,0x07E9,0x07E8,0x07E7,0x07E5,0x07E4,0x07E3,
+  0x07E1,0x07E0,0x07E0,0x17E0,0x1FE0,0x27E0,0x37E0,0x3FE0,0x4FE0,0x57E0,
+  0x5FE0,0x6FE0,0x77E0,0x7FE0,0x8FE0,0x97E0,0x9FE0,0xAFE0,0xB7E0,0xBFE0,
+  0xCFE0,0xD7E0,0xDFE0,0xEFE0,0xF7E0,0xFFE0,0xFFE0,0xFFC0,0xFFA0,0xFF80,
+  0xFF80,0xFF60,0xFF40,0xFF20,0xFF20,0xFF00,0xFEE0,0xFEE0,0xFEC0,0xFEA0,
+  0xFE80,0xFE80,0xFE60,0xFE40,0xFE40,0xFE20,0xFE00,0xFDE0,0xFDE0,0xFDC0,
+  0xFDA0,0xFD80,0xFD80,0xFD60,0xFD40,0xFD40,0xFD20,0xFD00,0xFCE0,0xFCE0,
+  0xFCC0,0xFCA0,0xFCA0,0xFC80,0xFC60,0xFC40,0xFC40,0xFC20,0xFC00,0xFBE0,
+  0xFBE0,0xFBC0,0xFBA0,0xFBA0,0xFB80,0xFB60,0xFB40,0xFB40,0xFB20,0xFB00,
+  0xFB00,0xFAE0,0xFAC0,0xFAA0,0xFAA0,0xFA80,0xFA60,0xFA40,0xFA40,0xFA20,
+  0xFA00,0xFA00,0xF9E0,0xF9C0,0xF9A0,0xF9A0,0xF980,0xF960,0xF960,0xF940,
+  0xF920,0xF900,0xF900,0xF8E0,0xF8C0,0xF8A0,0xF8A0,0xF880,0xF860,0xF860,
+  0xF840,0xF820,0xF800,0xF800,0xF800,0xF801,0xF802,0xF802,0xF803,0xF804,
+  0xF804,0xF805,0xF806,0xF807,0xF807,0xF808,0xF809,0xF80A,0xF80A,0xF80B,
+  0xF80C,0xF80D,0xF80D,0xF80E,0xF80F,0xF810,0xF810,0xF811,0xF812,0xF812,
+  0xF813,0xF814,0xF815,0xF815,0xF816,0xF817,0xF818,0xF818,0xF819,0xF81A,
+  0xF81B,0xF81B,0xF81C,0xF81D,
+};
 
 void drawPicture() {
-  if (INTERPOLATE) {
-    interpolate();
-    for (y=0; y<O_HEIGHT; y++) {
-      for (x=0; x<O_WIDTH; x++) {
-        imageData[(y*O_WIDTH) + x] = getColor(interpolated[y][x]);
-      }
-    }
-    Display.pushImage(8, 8, O_WIDTH, O_HEIGHT, imageData);
-  }
-  else {
-    for (y=0; y<24; y++) {
-      for (x=0; x<32; x++) {
-        Display.fillRect(8 + x*7, 8 + y*7, 7, 7, getColor(tempValues[(31-x) + (y*32)]));
-      }
+  float degreesPerColor = 223.0 / (maxTemp - minTemp);
+  for (y=0; y<24; y++) {
+    for (x=0; x<32; x++) {
+      float t = tempValues[(31-x) + (y*32)];
+      // uint8_t colorIndex = map(t, minTemp, maxTemp, 0, sizeof(camColors)/sizeof(uint16_t) - 1);
+      uint8_t colorIndex = (t - minTemp) * degreesPerColor;
+      Display.fillRect(8 + x*7, 8 + y*7, 7, 7, camColors[colorIndex]);
     }
   }
 }
-
-// Get color for temp value.
-uint16_t getColor(float val) {
-  /*
-    pass in value and figure out R G B
-    several published ways to do this I basically graphed R G B and developed simple linear equations
-    again a 5-6-5 color display will not need accurate temp to R G B color calculation
-
-    equations based on
-    http://web-tech.ga-usa.com/2012/05/creating-a-custom-hot-to-cold-temperature-color-gradient-for-use-with-rrdtool/index.html
-
-  */
-
-  red = constrain(255.0 / (c - b) * val - ((b * 255.0) / (c - b)), 0, 255);
-
-  if ((val > minTemp) & (val < a)) {
-    green = constrain(255.0 / (a - minTemp) * val - (255.0 * minTemp) / (a - minTemp), 0, 255);
-  }
-  else if ((val >= a) & (val <= c)) {
-    green = 255;
-  }
-  else if (val > c) {
-    green = constrain(255.0 / (c - d) * val - (d * 255.0) / (c - d), 0, 255);
-  }
-  else if ((val > d) | (val < a)) {
-    green = 0;
-  }
-
-  if (val <= b) {
-    blue = constrain(255.0 / (a - b) * val - (255.0 * b) / (a - b), 0, 255);
-  }
-  else if ((val > b) & (val <= d)) {
-    blue = 0;
-  }
-  else if (val > d) {
-    blue = constrain(240.0 / (maxTemp - d) * val - (d * 240.0) / (maxTemp - d), 0, 240);
-  }
-
-  // use the displays color mapping function to get 5-6-5 color palet (R=5 bits, G=6 bits, B-5 bits)
-  return Display.color565(red, green, blue);
-}
-
 
 void setTempScale() {
-  minTemp = 255;
-  maxTemp = 0;
+  minTemp = 1000;
+  maxTemp = -1000;
 
   for (i = 0; i < 768; i++) {
     minTemp = min(minTemp, tempValues[i]);
     maxTemp = max(maxTemp, tempValues[i]);
   }
-
-  setAbcd();
-  drawLegend();
 }
 
-
-// Function to get the cutoff points in the temp vs RGB graph.
-void setAbcd() {
-  a = minTemp + (maxTemp - minTemp) * 0.2121;
-  b = minTemp + (maxTemp - minTemp) * 0.3182;
-  c = minTemp + (maxTemp - minTemp) * 0.4242;
-  d = minTemp + (maxTemp - minTemp) * 0.8182;
+void drawRainbow() {
+  for (i = 0; i < 224; i += 1) {
+    Display.drawFastVLine(8+ + i, 292, 20, camColors[i]);
+  }
 }
-
 
 // Draw a legend.
 void drawLegend() {
-  float inc = (maxTemp - minTemp) / 224.0;
-  j = 0;
-  for (ii = minTemp; ii < maxTemp; ii += inc) {
-    Display.drawFastVLine(8+ + j++, 292, 20, getColor(ii));
-  }
-
   Display.setTextFont(2);
   Display.setTextSize(1);
   Display.setCursor(8, 272);
